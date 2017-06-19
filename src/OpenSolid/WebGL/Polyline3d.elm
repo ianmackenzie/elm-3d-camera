3ebc5480252d79493a23693815f70137eeb795af
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-06-19 18:26:04 -0400

    Add Polyline3d.toScreenSpace

--- src/OpenSolid/WebGL/Polyline3d.elm
+++ src/OpenSolid/WebGL/Polyline3d.elm
@@ -1,14 +1,23 @@
 module OpenSolid.WebGL.Polyline3d
     exposing
-        ( vertexPositions
+        ( toScreenSpace
+        , vertexPositions
         )
 
 import Math.Vector3 exposing (Vec3)
 import OpenSolid.Geometry.Types exposing (..)
 import OpenSolid.Polyline3d as Polyline3d
+import OpenSolid.WebGL.Camera exposing (Camera)
 import OpenSolid.WebGL.Point3d as Point3d
 
 
 vertexPositions : Polyline3d -> List { vertexPosition : Vec3 }
 vertexPositions polyline =
     List.map Point3d.toVertexPosition (Polyline3d.vertices polyline)
+
+
+toScreenSpace : Camera -> Polyline3d -> Polyline2d
+toScreenSpace camera polyline =
+    Polyline3d.vertices polyline
+        |> List.map (Point3d.toScreenSpace camera)
+        |> Polyline2d
