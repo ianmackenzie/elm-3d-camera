450c59c02d6c0695d3ef00977c1c2241be27b0a8
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-06-19 18:25:53 -0400

    Update to elm-format 0.7.0-exp

--- src/OpenSolid/WebGL/Polyline3d.elm
+++ src/OpenSolid/WebGL/Polyline3d.elm
@@ -3,10 +3,10 @@ module OpenSolid.WebGL.Polyline3d
         ( vertexPositions
         )
 
+import Math.Vector3 exposing (Vec3)
 import OpenSolid.Geometry.Types exposing (..)
 import OpenSolid.Polyline3d as Polyline3d
 import OpenSolid.WebGL.Point3d as Point3d
-import Math.Vector3 exposing (Vec3)
 
 
 vertexPositions : Polyline3d -> List { vertexPosition : Vec3 }
