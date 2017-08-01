0f65754dd454c4dbdc27db91e33ce731ed4a0a0e
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-08-01 14:23:21 -0400

    Remove 'vertexPositions' functions
    Awkwardly named, and don't really fit into either opensolid/linear-algebra (soon linear-algebra-interop) or the planned opensolid/camera package

--- src/OpenSolid/WebGL/Triangle3d.elm
+++ src/OpenSolid/WebGL/Triangle3d.elm
@@ -1,8 +1,6 @@
 module OpenSolid.WebGL.Triangle3d
     exposing
         ( toScreenSpace
-        , vertexPositions
-        , vertexPositionsAndNormals
         )
 
 import Math.Vector3 as Vector3 exposing (Vec3)
@@ -11,32 +9,6 @@ import OpenSolid.Triangle3d as Triangle3d
 import OpenSolid.WebGL.Camera exposing (Camera)
 import OpenSolid.WebGL.Direction3d as Direction3d
 import OpenSolid.WebGL.Point3d as Point3d
-
-
-vertexPositions : Triangle3d -> ( Vec3, Vec3, Vec3 )
-vertexPositions triangle =
-    let
-        ( p1, p2, p3 ) =
-            Triangle3d.vertices triangle
-    in
-    ( Point3d.toVec3 p1, Point3d.toVec3 p2, Point3d.toVec3 p3 )
-
-
-vertexPositionsAndNormals : Triangle3d -> ( ( Vec3, Vec3 ), ( Vec3, Vec3 ), ( Vec3, Vec3 ) )
-vertexPositionsAndNormals triangle =
-    let
-        ( p1, p2, p3 ) =
-            Triangle3d.vertices triangle
-
-        normalVector =
-            Triangle3d.normalDirection triangle
-                |> Maybe.map Direction3d.toVec3
-                |> Maybe.withDefault (Vector3.vec3 0 0 0)
-    in
-    ( ( Point3d.toVec3 p1, normalVector )
-    , ( Point3d.toVec3 p2, normalVector )
-    , ( Point3d.toVec3 p3, normalVector )
-    )
 
 
 toScreenSpace : Camera -> Triangle3d -> Triangle2d
