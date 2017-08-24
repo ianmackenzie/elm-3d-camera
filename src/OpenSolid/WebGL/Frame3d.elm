f31211ffef6713cd33e7a9eb5c1454f4dca3c3c1
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-08-23 22:55:41 -0400

    Complete initial Frame3d documentation

--- src/OpenSolid/WebGL/Frame3d.elm
+++ src/OpenSolid/WebGL/Frame3d.elm
@@ -7,7 +7,13 @@ module OpenSolid.WebGL.Frame3d
         )
 
 {-| Functions for constructing WebGL model and view matrices from `Frame3d`
-values.
+values, and for constructing frames useful for defining camera positions and
+orientations.
+
+Note that according to OpenGL convention, the view direction of the camera is
+the _negative_ Z direction of its frame. The positive Z direction of the frame
+is 'out of the screen', the positive X direction is to the right and the
+positive Y direction is up.
 
 @docs modelMatrix, viewMatrix, modelViewMatrix, lookAt
 
@@ -36,11 +42,6 @@ modelMatrix frame =
 from a `Frame3d` that defines the position and orientation of a camera.
 Multiplying by this matrix transforms from world coordinates to eye coordinates.
 
-Note that according to OpenGL convention, the view direction is the _negative_ Z
-direction of the frame. The positive Z direction of the frame is 'out of the
-screen', the positive X direction is to the right and the positive Y direction
-is up.
-
 Note that you will typically also need a [projection matrix](http://www.opengl-tutorial.org/beginners-tutorials/tutorial-3-matrices/#the-projection-matrix);
 the `linear-algebra` library provides [several](http://package.elm-lang.org/packages/elm-community/linear-algebra/latest/Math-Matrix4#projections).
 
@@ -66,7 +67,28 @@ modelViewMatrix eyeFrame modelFrame =
     Frame3d.toMat4 (Frame3d.relativeTo eyeFrame modelFrame)
 
 
-{-| -}
+{-| Construct a `Frame3d` that can be used as the camera frame for a camera at
+the given eye point looking towards the given focal point, with the given
+global up direction (which will typically be `Direction3d.positiveZ` or
+`Direction3d.positiveY`). That is likely all you need to know but if you are
+interested in the details, read on!
+
+The Z direction of the returned frame will point _from_ the focal point _to_ the
+eye point (since as mentioned above OpenGL expects the viewing direction to be
+the _negative_ Z direction). The Y direction will be chosen to be as close to
+the global up direction as possible (the camera will not 'roll') and the X
+direction will point to the right.
+
+If the direction from the eye point to the focal point is parallel to the global
+up direction (that is, the camera is pointing straight up or straight down) then
+the X and Y directions will be chosen arbitrarily.
+
+If the given eye point and focal point are coincident (so that there is no well-
+defined viewing direction), then the returned frame will have its Y direction
+set to the global up direction and its X and Z directions will be chosen
+arbitrarily.
+
+-}
 lookAt : { focalPoint : Point3d, eyePoint : Point3d, upDirection : Direction3d } -> Frame3d
 lookAt { focalPoint, eyePoint, upDirection } =
     let
