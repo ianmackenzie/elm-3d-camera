baab972982bda6699122f83bc997896ae796ddcb
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-03-08 17:04:32 -0400

    Add test suite
    Compare matrix-based operations to toScreenSpace functions

---
+++ tests/Camera3d/Fuzz.elm
@@ -0,0 +1,66 @@
+module Camera3d.Fuzz exposing (ScreenCoordinates, WorldCoordinates, camera, screen, viewpoint)
+
+import Angle
+import Camera3d exposing (Camera3d)
+import Camera3d.Types as Types
+import Fuzz exposing (Fuzzer)
+import Geometry.Fuzz as Fuzz
+import Length exposing (Meters)
+import Pixels exposing (Pixels)
+import Point2d
+import Point3d exposing (Point3d)
+import Quantity exposing (Quantity)
+import Rectangle2d exposing (Rectangle2d)
+import Viewpoint3d exposing (Viewpoint3d)
+
+
+type WorldCoordinates
+    = WorldCoordinates
+
+
+viewpoint : Fuzzer (Viewpoint3d Meters WorldCoordinates)
+viewpoint =
+    Fuzz.map Types.Viewpoint3d Fuzz.frame3d
+
+
+camera : Fuzzer (Camera3d Meters WorldCoordinates)
+camera =
+    Fuzz.oneOf
+        [ Fuzz.map2
+            (\viewpoint_ verticalFieldOfView ->
+                Camera3d.perspective
+                    { viewpoint = viewpoint_
+                    , verticalFieldOfView = verticalFieldOfView
+                    }
+            )
+            viewpoint
+            (Fuzz.quantityRange (Angle.degrees 1) (Angle.degrees 179))
+        , Fuzz.map2
+            (\viewpoint_ viewportHeight ->
+                Camera3d.orthographic
+                    { viewpoint = viewpoint_
+                    , viewportHeight = viewportHeight
+                    }
+            )
+            viewpoint
+            (Fuzz.quantityRange (Length.millimeters 1) (Length.meters 10))
+        ]
+
+
+type ScreenCoordinates
+    = ScreenCoordinates
+
+
+screenDimension : Fuzzer (Quantity Float Pixels)
+screenDimension =
+    Fuzz.quantityRange (Pixels.pixels 1) (Pixels.pixels 1000)
+
+
+screen : Fuzzer (Rectangle2d Pixels ScreenCoordinates)
+screen =
+    Fuzz.map2
+        (\width height ->
+            Rectangle2d.from Point2d.origin (Point2d.xy width height)
+        )
+        screenDimension
+        screenDimension
---
+++ tests/Tests.elm
@@ -0,0 +1,256 @@
+module Tests exposing (suite)
+
+import Camera3d exposing (Camera3d)
+import Camera3d.Fuzz as Fuzz exposing (ScreenCoordinates, WorldCoordinates)
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer)
+import Geometry.Expect as Expect
+import Geometry.Fuzz as Fuzz
+import Geometry.Interop.LinearAlgebra.Point3d as Point3d
+import Length exposing (Meters)
+import Pixels exposing (Pixels)
+import Plane3d
+import Point2d exposing (Point2d)
+import Point3d exposing (Point3d)
+import Point3d.Projection as Point3d
+import Quantity exposing (Quantity, Unitless)
+import Rectangle2d exposing (Rectangle2d)
+import SketchPlane3d
+import Test exposing (Test)
+import Viewpoint3d
+
+
+type NdcCoordinates
+    = NdcCoordinates
+
+
+ndcToScreen :
+    Rectangle2d Pixels ScreenCoordinates
+    -> Point3d Unitless NdcCoordinates
+    -> Point2d Pixels ScreenCoordinates
+ndcToScreen screenRectangle ndcPoint =
+    let
+        ( ndcX, ndcY, _ ) =
+            Point3d.toTuple Quantity.toFloat ndcPoint
+    in
+    Rectangle2d.interpolate screenRectangle
+        ((ndcX + 1) / 2)
+        ((ndcY + 1) / 2)
+
+
+validPoint : Camera3d Meters WorldCoordinates -> Point3d Meters WorldCoordinates -> Bool
+validPoint camera point =
+    let
+        viewpoint =
+            Camera3d.viewpoint camera
+
+        eyePoint =
+            Viewpoint3d.eyePoint viewpoint
+
+        viewDirection =
+            Viewpoint3d.viewDirection viewpoint
+
+        viewPlane =
+            Plane3d.through eyePoint viewDirection
+    in
+    Point3d.signedDistanceFrom viewPlane point |> Quantity.greaterThan (Length.meters 0.1)
+
+
+projectionParameters :
+    Rectangle2d Pixels coordinates
+    ->
+        { nearClipDistance : Quantity Float Meters
+        , farClipDistance : Quantity Float Meters
+        , aspectRatio : Float
+        }
+projectionParameters screen =
+    let
+        ( width, height ) =
+            Rectangle2d.dimensions screen
+    in
+    { nearClipDistance = Length.meters 0.1
+    , farClipDistance = Length.meters 100
+    , aspectRatio = Quantity.ratio width height
+    }
+
+
+genericTestCase : Fuzzer { camera : Camera3d Meters WorldCoordinates, point : Point3d Meters pointCoordinates, screen : Rectangle2d Pixels ScreenCoordinates }
+genericTestCase =
+    Fuzz.map3
+        (\camera point screen ->
+            { camera = camera
+            , point = point
+            , screen = screen
+            }
+        )
+        Fuzz.camera
+        Fuzz.point3d
+        Fuzz.screen
+
+
+rayTestCase :
+    Fuzzer
+        { camera : Camera3d Meters WorldCoordinates
+        , point2d : Point2d Pixels ScreenCoordinates
+        , screen : Rectangle2d Pixels ScreenCoordinates
+        , distance : Quantity Float Meters
+        }
+rayTestCase =
+    Fuzz.map4
+        (\camera screen ( u, v ) distance ->
+            { camera = camera
+            , point2d = Rectangle2d.interpolate screen u v
+            , screen = screen
+            , distance = distance
+            }
+        )
+        Fuzz.camera
+        Fuzz.screen
+        (Fuzz.tuple ( Fuzz.parameterValue, Fuzz.parameterValue ))
+        (Fuzz.quantityRange (Length.meters 1) (Length.meters 10))
+
+
+suite : Test
+suite =
+    Test.describe "elm-3d-camera"
+        [ Test.fuzz genericTestCase
+            "viewMatrix/projectionMatrix and toScreenSpace are consistent"
+            (\{ camera, point, screen } ->
+                if validPoint camera point then
+                    let
+                        firstPoint =
+                            Point3d.toScreenSpace camera screen point
+
+                        viewMatrix =
+                            Camera3d.viewMatrix camera
+
+                        projectionMatrix =
+                            Camera3d.projectionMatrix
+                                (projectionParameters screen)
+                                camera
+
+                        secondPoint =
+                            point
+                                |> Point3d.transformBy viewMatrix
+                                |> Point3d.transformBy projectionMatrix
+                                |> ndcToScreen screen
+                    in
+                    firstPoint |> Expect.point2d secondPoint
+
+                else
+                    Expect.pass
+            )
+        , Test.fuzz genericTestCase
+            "viewProjectionMatrix and toScreenSpace are consistent"
+            (\{ camera, point, screen } ->
+                if validPoint camera point then
+                    let
+                        firstPoint =
+                            Point3d.toScreenSpace camera screen point
+
+                        viewProjectionMatrix =
+                            Camera3d.viewProjectionMatrix
+                                (projectionParameters screen)
+                                camera
+
+                        secondPoint =
+                            point
+                                |> Point3d.transformBy viewProjectionMatrix
+                                |> ndcToScreen screen
+                    in
+                    firstPoint |> Expect.point2d secondPoint
+
+                else
+                    Expect.pass
+            )
+        , Test.fuzz2
+            genericTestCase
+            Fuzz.frame3d
+            "modelViewMatrix/projectionMatrix and placeIn/toScreenSpace are consistent"
+            (\{ camera, point, screen } modelFrame ->
+                let
+                    globalPoint =
+                        Point3d.placeIn modelFrame point
+                in
+                if validPoint camera globalPoint then
+                    let
+                        firstPoint =
+                            Point3d.toScreenSpace camera screen globalPoint
+
+                        modelViewMatrix =
+                            Camera3d.modelViewMatrix modelFrame camera
+
+                        projectionMatrix =
+                            Camera3d.projectionMatrix
+                                (projectionParameters screen)
+                                camera
+
+                        secondPoint =
+                            point
+                                |> Point3d.transformBy modelViewMatrix
+                                |> Point3d.transformBy projectionMatrix
+                                |> ndcToScreen screen
+                    in
+                    firstPoint |> Expect.point2d secondPoint
+
+                else
+                    Expect.pass
+            )
+        , Test.fuzz2
+            genericTestCase
+            Fuzz.frame3d
+            "modelViewMatrix/projectionMatrix and modelViewProjectionMatrix are consistent"
+            (\{ camera, point, screen } modelFrame ->
+                let
+                    globalPoint =
+                        Point3d.placeIn modelFrame point
+                in
+                if validPoint camera globalPoint then
+                    let
+                        modelViewMatrix =
+                            Camera3d.modelViewMatrix modelFrame camera
+
+                        projectionMatrix =
+                            Camera3d.projectionMatrix
+                                (projectionParameters screen)
+                                camera
+
+                        modelViewProjectionMatrix =
+                            Camera3d.modelViewProjectionMatrix
+                                modelFrame
+                                (projectionParameters screen)
+                                camera
+
+                        firstPoint =
+                            point
+                                |> Point3d.transformBy modelViewMatrix
+                                |> Point3d.transformBy projectionMatrix
+                                |> ndcToScreen screen
+
+                        secondPoint =
+                            point
+                                |> Point3d.transformBy modelViewProjectionMatrix
+                                |> ndcToScreen screen
+                    in
+                    firstPoint |> Expect.point2d secondPoint
+
+                else
+                    Expect.pass
+            )
+        , Test.fuzz
+            rayTestCase
+            "ray and toScreenSpace are consistent"
+            (\{ camera, screen, point2d, distance } ->
+                let
+                    ray =
+                        Camera3d.ray camera screen point2d
+
+                    point3d =
+                        Point3d.along ray distance
+
+                    projectedPoint =
+                        Point3d.toScreenSpace camera screen point3d
+                in
+                projectedPoint |> Expect.point2d point2d
+            )
+        ]
